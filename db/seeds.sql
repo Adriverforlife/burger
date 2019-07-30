29ff58af3123b2b63fe61f39fe0ace50876dc62d
Michael Eguina <michael@corner-exit.com>
2019-07-29 17:34:32 -0700

    ignore added

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Ice Cream and Jelly Burger');
+INSERT INTO burgers (burger_name) VALUES ('Fire Burger with Bacon!');
